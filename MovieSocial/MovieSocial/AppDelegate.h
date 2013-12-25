e2aaef73ed0171ac5a0020bfe326580bd1d5d020
xrxxiaohui <leexiaohuidt@163.com>
2013-12-25 12:32:19 +0800

    测试

---
+++ MovieSocial/MovieSocial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieSocial
+//
+//  Created by Apple on 13-12-25.
+//  Copyright (c) 2013年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
